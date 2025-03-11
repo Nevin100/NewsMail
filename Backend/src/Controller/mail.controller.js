3506ad7dbeb15b166d882ae58a4a3f21715f50b8
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-03-11 21:07:52 +0530

    Completed NewsLetter

--- Backend/src/Controller/mail.controller.js
+++ Backend/src/Controller/mail.controller.js
@@ -1,4 +1,6 @@
 import Mail from "../Model/mail.model.js";
+import { Parser } from "json2csv";
+import fs from "fs";
 
 export const mailsLetter = async (req, res) => {
   const { mail } = req.body;
@@ -22,15 +24,29 @@ export const mailsLetter = async (req, res) => {
 
     await NewMail.save();
 
-    res
-      .status(200)
-      .json({
-        message: "Mail registered successfully",
-        error: false,
-        data: NewMail,
-      });
+    res.status(200).json({
+      message: "Mail registered successfully",
+      error: false,
+      data: NewMail,
+    });
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: "Internal Server Issue", error: true });
+  }
+};
+
+export const CSVDownload = async (req, res) => {
+  try {
+    const data = await Mail.find().lean();
+    const fields = Object.keys(data[0]);
+    const json2csvParser = new Parser({ fields });
+    const csv = json2csvParser.parse(data);
+
+    res.header("Content-Type", "text/csv");
+    res.attachment("data.csv");
+    return res.send(csv);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error exporting data");
   }
 };
--- Backend/src/Routes/mail.Route.js
+++ Backend/src/Routes/mail.Route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { mailsLetter } from "../Controller/mail.controller.js";
+import { mailsLetter, CSVDownload } from "../Controller/mail.controller.js";
 
 const router = express.Router();
 
 //Mail Routes :
 router.post("/mails-newsletter", mailsLetter);
+
+//Excel-sheet
+router.get("/export-csv", CSVDownload);
 
 export default router;
