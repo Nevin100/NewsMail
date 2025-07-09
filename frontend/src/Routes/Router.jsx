7be582ca59429fe54d157a4dc7cb3981258f1436
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-07-09 11:45:51 +0530

    Fixing Deployment Issues

--- frontend/src/Routes/Router.jsx
+++ frontend/src/Routes/Router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import NewsLetterPage from "../Pages/NewsLetterPage.jsx";
 import App from "../App.jsx";
 import Settings from "../Pages/Settings.jsx";
-import ProtectedRoute from "../Context/Protectedroute.jsx";
+import ProtectedRoute from "../Context/ProtectedRoute.jsx";
 import AdminDashboard from "../Components/Admin/AdminDashboard.jsx";
 import AdminLogin from "../Components/Admin/AdminLogin.jsx";
 import About from "../Pages/About.jsx";
