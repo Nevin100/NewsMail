2c3bff1ecea69d01db4353db03856eec1cf4fbba
Nevin100 <opopopopop100000@gmail.com>
2026-01-12 17:26:16 +0530

    Completed UI Issue

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -35,6 +35,18 @@ const Navbar = () => {
             </Link>
           </div>
 
+          <div className="flex items-center gap-8">
+            <Link
+              to="/admin"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <FcAbout className="w-5 h-5 text-primary" />
+              </div>
+              <span className="hidden sm:inline">About</span>
+            </Link>
+          </div>
+
           <div className="flex items-center gap-2">
             <Link
               to={"/settings"}
