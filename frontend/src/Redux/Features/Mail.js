2faf19526bd549b2ab21d605f75a52be83dc6b98
Nevin100 <opopopopop100000@gmail.com>
2026-09-04 17:38:20 +0530

    Completing and fixing the Prod -III

--- frontend/src/Redux/Features/Mail.js
+++ frontend/src/Redux/Features/Mail.js
@@ -3,19 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseUrl = import.meta.env.VITE_API_URL || "";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${baseUrl}/news-mail`,
+  baseUrl: `${baseUrl}`,
   credentials: "include",
 });
 
 const MailsApi = createApi({
   reducerPath: "MailsApi",
-  baseQuery,
+  baseQuery,  
   tagTypes: ["Mails"],
   endpoints: (builder) => ({
     //Add Mail
     AddMail: builder.mutation({
       query: (newMail) => ({
-        url: `/mails-newsletter`,
+        url: `/news-mail/mails-newsletter`,
         method: "POST",
         body: newMail,
       }),
